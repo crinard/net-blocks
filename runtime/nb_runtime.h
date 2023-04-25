b1291cf59a5bc97cf3f40fe24833aacf304e54e8
Chris Rinard <chris.e.rinard@gmail.com>
2023-04-25 08:00:53 -0400

    deleted runtime, oops

---
+++ runtime/nb_runtime.h
@@ -0,0 +1,91 @@
+#ifndef NB_RUNTIME_H
+#define NB_RUNTIME_H
+#include <stdlib.h>
+#include <string.h>
+#include "gen_headers.h"
+#include "nb_timer.h"
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+#define QUEUE_EVENT_ESTABLISHED (0)
+#define QUEUE_EVENT_READ_READY (1)
+#define QUEUE_EVENT_ACCEPT_READY (2)
+
+static int nb__connection_t_size(void) {
+	return sizeof(nb__connection_t);
+}
+
+// Data queue methods
+struct data_queue_t* nb__new_data_queue(void);
+void nb__free_data_queue(struct data_queue_t*);
+void nb__insert_data_queue(struct data_queue_t*, char*, int);
+
+// Accept queue methods
+nb__accept_queue_t* nb__new_accept_queue(void);
+void nb__free_accept_queue(nb__accept_queue_t*);
+void nb__insert_accept_queue(nb__accept_queue_t*, unsigned, char*, void*);
+
+void nb__add_connection(nb__connection_t*, unsigned sa);
+void nb__delete_connection(unsigned sa);
+nb__connection_t* nb__retrieve_connection(unsigned sa);
+
+
+void nb__debug_packet(char* p);
+
+
+// Network interface API TODO: implement these -- they have to interface with the 
+// bottom of the stack. 
+// The top (application layer is written using the code (like follows)), and the 
+// application will have the netblocks implementations.
+/**
+ * nb__ipc_init("/tmp/ipc_socket", 0);
+	printf("IPC initialized\n");
+	nb__net_init();
+	memcpy(nb__my_host_id, client_id, 6);
+
+	nb__connection_t * conn = nb__establish(server_id, 8080, 8081, callback);
+	
+	nb__send(conn, CLIENT_MSG, sizeof(CLIENT_MSG));
+
+	while (running) {
+		nb__main_loop_step();
+		usleep(100 * 1000);
+	}
+	nb__destablish(conn);
+ */
+char* nb__poll_packet(int*, int);
+int nb__send_packet(char*, int);
+void nb__ipc_init(const char* sock_path, int mode);
+void nb__ipc_deinit();
+void nb__mlx5_init(void);
+char* nb__request_send_buffer(void);
+void* nb__return_send_buffer(char*);
+
+
+// Generated protocol API
+void nb__run_ingress_step (void*, int);
+int nb__send (nb__connection_t* arg0, char* arg1, int arg2);
+void nb__destablish (nb__connection_t* arg0);
+nb__connection_t* nb__establish (char* arg0, unsigned int arg1, unsigned int arg2, void (*arg3)(int, nb__connection_t*));
+void nb__net_init (void);
+void nb__reliable_redelivery_timer_cb(nb__timer*, void* param, unsigned long long to);
+
+// Runtime API
+int nb__read(nb__connection_t*, char*, int);
+nb__connection_t* nb__accept(nb__connection_t*, void (*)(int, nb__connection_t*));
+void nb__main_loop_step(void);
+
+extern char nb__reuse_mtu_buffer[];
+extern char nb__my_host_id[];
+extern nb__net_state_t* nb__net_state;
+
+extern char nb__wildcard_host_identifier[];
+
+
+extern unsigned long long nb__get_time_ms_now(void);
+#ifdef __cplusplus 
+}
+#endif
+
+#endif
